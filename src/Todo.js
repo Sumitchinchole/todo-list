b25b811f7245d5e3056a0a3bed496d1af63fee38
Sumitchinchole <smtchinchole@gamil.com>
2022-03-16 12:05:44 +0530

    React Hook form Changes

--- src/Todo.js
+++ src/Todo.js
@@ -1,29 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addTodo, deleteTodo } from "./actions/index";
-import { Button, List, Form, Icon, Header } from "semantic-ui-react";
-
-// Add ToDo in Local Storage
-
-// const getLocalItems = () => {
-//   let list = localStorage.getItem("lists");
-
-//   if (list) {
-//     return JSON.parse(localStorage.getItem("lists"));
-//   } else {
-//     return [];
-//   }
-// };
+import { List, Form, Icon, Header } from "semantic-ui-react";
+import { useForm, Controller } from "react-hook-form";
 
 function ToDo() {
-  const [InputList, setInputList] = useState("");
+  // const [InputList, setInputList] = useState("");
   const list = useSelector((state) => state.todoReducers.list);
   //const [Items, setItems] = useState([]);
   const dispatch = useDispatch();
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({
+    defaultValues: { todo: "" },
+  });
+  console.log(errors);
 
-  // useEffect(() => {
-  //   localStorage.setItem("lists", JSON.stringify(list));
-  // }, [list]);
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({ todo: "" });
+    }
+  }, [isSubmitSuccessful, reset]);
+
+  const onSubmit = ({ todo }) => {
+    dispatch(addTodo(todo));
+    console.log(todo);
+  };
 
   return (
     <div className="main_div">
@@ -32,22 +37,30 @@ function ToDo() {
           First Header
         </Header>
         <br />
-        <Form>
-          <Form.Input
-            focus
-            error={{ content: "Please enter your work", pointing: "below" }}
-            type="text"
-            placeholder="Add a Items"
-            onChange={(e) => setInputList(e.target.value)}
-            value={InputList}
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            control={control}
+            name="todo"
+            render={({ field }) => (
+              <Form.Input
+                focus
+                error={errors.todo && { content: errors.todo.message }}
+                type="text"
+                placeholder="Add a Items"
+                onBlur={field.onBlur}
+                onChange={field.onChange}
+                value={field.value}
+              />
+            )}
           />
+          <Form.Button
+            primary
+            //onClick={() => dispatch(addTodo(InputList), setInputList(""))}
+          >
+            ADD
+          </Form.Button>
         </Form>
-        <Button
-          primary
-          onClick={() => dispatch(addTodo(InputList), setInputList(""))}
-        >
-          ADD
-        </Button>
+
         <List>
           {list.map((elem) => {
             return (
